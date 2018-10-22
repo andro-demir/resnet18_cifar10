ca9687c3b1cdce1bda764110ef3e1271e2d58728
andac-demir <andacdemir@hotmail.com>
2018-10-21 21:18:05 -0400

    resnet18

--- CNNclassify.py
+++ CNNclassify.py
@@ -9,13 +9,6 @@ import torch.optim as optim
 import torchvision
 import matplotlib.pyplot as plt
 
-"""
-    ks x ks convolution with padding
-"""
-def conv(in_ch, out_ch, ks, stride=1):
-    return nn.Conv2d(in_ch, out_ch, kernel_size=ks, stride=stride,
-                     padding=1, bias=False)
-
 class basic_block(nn.Module):
     expansion = 1
 
@@ -44,37 +37,6 @@ class basic_block(nn.Module):
         return out
 
 
-class bottleneck(nn.Module):
-    expansion = 4
-
-    def __init__(self, in_planes, planes, stride=1):
-        super(bottleneck, self).__init__()
-        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(planes)
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride, 
-                               padding=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(planes)
-        self.conv3 = nn.Conv2d(planes, self.expansion*planes, kernel_size=1, 
-                               bias=False)
-        self.bn3 = nn.BatchNorm2d(self.expansion*planes)
-
-        self.shortcut = nn.Sequential()
-        if stride != 1 or in_planes != self.expansion*planes:
-            self.shortcut = nn.Sequential(nn.Conv2d(in_planes, 
-                                          self.expansion*planes, kernel_size=1, 
-                                          stride=stride, bias=False),
-                                          nn.BatchNorm2d(self.expansion*planes)
-                                         )
-
-    def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
-        out = F.relu(self.bn2(self.conv2(out)))
-        out = self.bn3(self.conv3(out))
-        out += self.shortcut(x)
-        out = F.relu(out)
-        return out
-
-
 '''
     Defines the network architecture, activations and regularizers.
     Forward prop.
@@ -85,19 +47,19 @@ class resnet(nn.Module):
                  num_blocks, 
                  num_classes=10):
         super(resnet, self).__init__()
-        self.in_planes = 64
-
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=7, 
-                               stride=(1,2,2), padding=(3,3,3), bias=False)
-        self.bn1 = nn.BatchNorm2d(num_features=64)
+        self.in_planes = 32
+        # First convolutional layer must have the kernel size 5x5,
+        # stride 1 and the total number of kernels 32, according to the 
+        # project assignment: 
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=5, 
+                               stride=1, padding=2, bias=False)
+        self.bn1 = nn.BatchNorm2d(num_features=32)
         self.activation = nn.ReLU(inplace=True)
-        self.maxpool = nn.MaxPool3d(kernel_size=(3,3,3), stride=2, padding=1)
-
-        self.layer1 = self.make_layer(block, 64, num_blocks[0], stride=1)
-        self.layer2 = self.make_layer(block, 128, num_blocks[1], stride=2)
-        self.layer3 = self.make_layer(block, 256, num_blocks[2], stride=2)
-        self.layer4 = self.make_layer(block, 512, num_blocks[3], stride=2)
-        self.dense = nn.Linear(512*block.expansion, num_classes)
+        self.layer1 = self.make_layer(block, 32, num_blocks[0], stride=1)
+        self.layer2 = self.make_layer(block, 64, num_blocks[1], stride=2)
+        self.layer3 = self.make_layer(block, 128, num_blocks[2], stride=2)
+        self.layer4 = self.make_layer(block, 256, num_blocks[3], stride=2)        
+        self.dense = nn.Linear(256*block.expansion, num_classes)
 
     def make_layer(self, block, planes, num_blocks, stride):
         strides = [stride] + [1]*(num_blocks-1)
@@ -111,7 +73,6 @@ class resnet(nn.Module):
         x = self.conv1(x)
         x = self.bn1(x)
         x = self.activation(x)
-        x = self.maxpool(x)
         
         x = self.layer1(x)
         x = self.layer2(x)
@@ -120,24 +81,12 @@ class resnet(nn.Module):
 
         x = F.avg_pool2d(x, 4)
         x = x.view(x.size(0), -1)
-        x = self.linear(x)
+        x = self.dense(x)
         return x
 
 
 def ResNet18():
     return resnet(basic_block, [2,2,2,2])
-
-def ResNet34():
-    return resnet(basic_block, [3,4,6,3])
-
-def ResNet50():
-    return resnet(bottleneck, [3,4,6,3])
-
-def ResNet101():
-    return resnet(bottleneck, [3,4,23,3])
-
-def ResNet152():
-    return resnet(bottleneck, [3,8,36,3])
 
 '''
     Sets the loss and optimization criterion and number of epochs.
